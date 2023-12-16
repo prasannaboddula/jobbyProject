a8c71081d66701dbea0f365ce37195f712214549
Prasanna <prasannaboddula749@gmail.com>
2023-12-16 15:48:17 +0530

    vercel.json

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const jobs = require("./jobsData.js");
+const jobs = require("./jobsData.json");
+const cors = require("cors");
 
 const app = express();
 const port = 9000;
 
+app.use(cors());
 app.get("/jobs", (req,res) => {
     res.send(jobs);
 });
